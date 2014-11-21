67140e65485a24b16e5a0712bfdab61365f67c57
Kacper Kowalik <xarthisius.kk@gmail.com>
2014-11-21 16:57:09 -0600

    Pass env to user

--- run.sh
+++ run.sh
@@ -3,8 +3,8 @@
 # Monitor mode for job control, allows fg in the last line
 set -o monitor
 
-# Reads RabbitMQ environment and starts server in the backgroun
-sudo -u nobody configurable-http-proxy --api-ip ${HOSTNAME} &> /var/log/chp.log &
+# Reads RabbitMQ environment and starts server in the background
+sudo -E -u nobody configurable-http-proxy --api-ip ${HOSTNAME} &> /var/log/chp.log &
 
 logfile=/var/log/chp.log
 
